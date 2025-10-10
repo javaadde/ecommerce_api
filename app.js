524d74e82b80ed3f02dc777184faf886dbc8a509
javad <iamkuttappiii@gmail.com>
2025-10-10 15:19:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
    cors({
-    origin: "https://javad-react.vercel.app/", // frontend
+    origin: "https://javad-react.vercel.app", // frontend
     credentials: true,
   })
 );
